80df67af9a7718e291f3c9ec44e8f468975b9a6a
Anup <haldaranup746@gmail.com>
2022-04-05 11:40:21 +0530

    Unit-4 day calculator assignment

---
+++ unit-4/sprint-1/day-1/assignments/calculator/main.js
@@ -0,0 +1,12 @@
+const add = require("./add")
+
+const multiply = require("./multiply")
+
+const divide = require("./divide")
+
+const subtract = require("./subtract")
+
+console.log(add(45,7))
+console.log(multiply(45,7))
+console.log(divide(45,7))
+console.log(subtract(45,7))
