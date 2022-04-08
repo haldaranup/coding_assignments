9278750e165eb6483482039b5ce67dcc510ef763
Anup <haldaranup746@gmail.com>
2022-04-08 19:53:06 +0530

    unit 3 assignment

---
+++ unit-3/sprint-2/evaluation/C2/scripts/checkout.js
@@ -0,0 +1,17 @@
+let order = document.getElementById("order-form");
+
+let address = document.createElement("input");
+
+let valueOrder = address.value;
+
+let btn = document.createElement("button");
+
+btn.addEventListener("click", function () {
+  setTimeout(function () {
+    alert("Order Successful");
+    window.location.href = "index.html";
+  }, 3000);
+});
+
+btn.textContent = "Order Now";
+order.append(address, btn);
