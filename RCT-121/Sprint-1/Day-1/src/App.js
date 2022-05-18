66783ae9af2c170f4456ca08bdc8ed4df26385e2
Anup <haldaranup746@gmail.com>
2022-05-18 11:09:22 +0530

    rct 121 day1

---
+++ RCT-121/Sprint-1/Day-1/src/App.js
@@ -0,0 +1,10 @@
+import Contact from "./Components/Contact";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
